2b8fd711bb9fcd5827882e2b861e93efe93fd08e
王明杰 <wmj1@meitu.com>
2018-02-07 11:29:43 +0800

    add

---
+++ index.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import { CLIEngine } from 'eslint';
+
+export default function reslint() {
+    var cli = new CLIEngine({
+        exclude: 'node_modules/**'
+    });
+    return {
+        name: 'reslint',
+        transform: (code, id) => {
+            let basePath = path.relative(process.cwd(), id).split(path.sep).join('/');
+            let relt = cli.executeOnText(code, basePath);
+
+            if (relt.results) {
+                let errTips = cli.getFormatter('stylish')(relt.results);
+                console.log(errTips);
+            }
+        }
+    }
+}
+
+
